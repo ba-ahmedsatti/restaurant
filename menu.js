43ed17a2e791585398293b143b899cd703d04518
ba-ahmedsatti <aihsatti@gmail.com>
2022-03-25 01:28:21 -0600

    Update json packages, and menu.js file

--- menu.js
+++ menu.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const MongoClient = require('mongodb').MongoClient;
+
 const app = express();
-const port = 8002;
+const port = 8003;
+const url = 'mongodb://localhost:27017';
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 
 app.set('view engine', 'pug');
 
