307c27d6a9614ba3f165ba51f31b69f2df3956a7
ba-ahmedsatti <aihsatti@gmail.com>
2022-04-04 01:31:50 -0600

    connecting mongo db

--- menu.js
+++ menu.js
@@ -23,18 +23,31 @@ app.use(cors({
 }));
 
 
-app.get('/menu', (req, res) => {
-  const menus = [
-    { id: 1, name: 'burger', price: 49, image: 'menu.jpg' },
-    { id: 2, name: 'salad', price: 39, image: 'menu1.jpg' },
-    { id: 3, name: 'shrimp', price: 30, image: 'menu3.jpg' },
-    { id: 4, name: 'pasta', price: 40, image: 'menu4.jpg' },
-    { id: 5, name: 'steak', price: 38, image: 'menu6.jpg' },
-    { id: 6, name: 'pizza', price: 45, image: 'menu7.jpg' },
-    { id: 7, name: 'fish', price: 33,image: 'menu8.jpg' },
-    { id: 8, name: 'ribeye', price: 46, image: 'menu9.jpg' }
-  ];
-  res.render('menu', { menus });
+
+const menus = [
+  { id: 1, name: 'burger', price: 49, image: 'menu.jpg' },
+  { id: 2, name: 'salad', price: 39, image: 'menu1.jpg' },
+  { id: 3, name: 'shrimp', price: 30, image: 'menu3.jpg' },
+  { id: 4, name: 'pasta', price: 40, image: 'menu4.jpg' },
+  { id: 5, name: 'steak', price: 38, image: 'menu6.jpg' },
+  { id: 6, name: 'pizza', price: 45, image: 'menu7.jpg' },
+  { id: 7, name: 'fish', price: 33,image: 'menu8.jpg' },
+  { id: 8, name: 'ribeye', price: 46, image: 'menu9.jpg' },
+]
+
+app.get('/', (req, res) => {
+  MongoClient.connect(url, function(err, client) {
+    const db = client.db('menus');
+    const collection = db.collection('menu');
+    collection.find({}).toArray((error, documents) => {
+      client.close();
+      res.render('menu', {menus: documents});
+    })
+  });
+});
+
+app.get('/menu/:menuId', (req, res) => {
+  res.render('menus', { menu: documents.find(menu => menu.id === req.params.menuId)})
 });
 
 app.get('/home', (req, res) => {
